9f26cfa31d8a760bb71dc48a1be0dc499761c033
Phillip Hayes <phillip@kloodle.com>
2016-12-31 19:16:54 +0000

    change requirements file

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'categories'
 ]
 
@@ -117,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/'
+
 
