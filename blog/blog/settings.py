c0451f5b1ee03d2afcc2b51211511fc05c6c18a8
Phillip Hayes <phillip@kloodle.com>
2016-12-31 19:54:37 +0000

    change requirements file

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = '/'
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
