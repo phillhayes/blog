dda97bd88762eb2f700647b17e63fbe88025ea0b
Phillip Hayes <phillip@kloodle.com>
2016-12-31 22:25:18 +0000

    trying to fix

--- app/categories/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-from .models import Category
-# Create your views here.
-def categories(request):
-    categories = Category.objects.all()
-    return render(request, 'categories/show-all.html', {'categories': categories})
