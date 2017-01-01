e9a62ec4e4c88bef5b6186103d9e857350113a4c
Phillip Hayes <phillip@kloodle.com>
2017-01-01 19:55:19 +0000

    new database settings

--- app/blog/settings/production.py
+++ app/blog/settings/production.py
@@ -1,3 +1,13 @@
 from blog.settings.base import *
 
 DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+    
+}
