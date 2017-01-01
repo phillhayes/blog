57cd068d0e6dba10cb659c6f2562ff123dcaa598
Phillip Hayes <phillip@kloodle.com>
2017-01-01 20:02:31 +0000

    new database settings

--- app/blog/settings/production.py
+++ app/blog/settings/production.py
@@ -6,7 +6,7 @@ DEBUG = False
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'default': database_url.config(
         default=config('postgres://klbmmkfsjdvcvu:bfa886f7ed4282f5ba334759bb83ba483c85fc7b62f51b7e1b29af2b690b0919@ec2-23-23-186-157.compute-1.amazonaws.com:5432/d413hlnpbnaa80')
     )
     
