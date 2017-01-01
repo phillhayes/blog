d5096dfdaeed546bd1f9c5df034d09beb53980b2
Phillip Hayes <phillip@kloodle.com>
2017-01-01 20:10:48 +0000

    new database settings

--- app/blog/settings/production.py
+++ app/blog/settings/production.py
@@ -8,6 +8,6 @@ DEBUG = False
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('postgres://klbmmkfsjdvcvu:bfa886f7ed4282f5ba334759bb83ba483c85fc7b62f51b7e1b29af2b690b0919@ec2-23-23-186-157.compute-1.amazonaws.com:5432/d413hlnpbnaa80')
+        default='postgres://klbmmkfsjdvcvu:bfa886f7ed4282f5ba334759bb83ba483c85fc7b62f51b7e1b29af2b690b0919@ec2-23-23-186-157.compute-1.amazonaws.com:5432/d413hlnpbnaa80')
     )
 }
