783175876f09d053259325e77d05200d5803a261
Phillip Hayes <phillip@kloodle.com>
2017-02-24 21:30:08 +0000

    add categories

--- app/blogpost/admin.py
+++ app/blogpost/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Blogpost
+from .models import Category
 
 # Register your models here.
 
 admin.site.register(Blogpost)
+admin.site.register(Category)
